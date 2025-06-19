b20658fc31128d23fe9fe611ed0afaf264025e25
Ben Lackey <ben.lackey@outlook.com>
2025-06-19 15:03:21 +0200

    Initial Commit

---
+++ open-feed-ios/open_feed_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  open_feed_iosApp.swift
+//  open-feed-ios
+//
+//  Created by Ben Lackey on 6/19/25.
+//
+
+import SwiftUI
+
+@main
+struct open_feed_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ open-feed-iosTests/open_feed_iosTests.swift
@@ -0,0 +1,17 @@
+//
+//  open_feed_iosTests.swift
+//  open-feed-iosTests
+//
+//  Created by Ben Lackey on 6/19/25.
+//
+
+import Testing
+@testable import open_feed_ios
+
+struct open_feed_iosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
